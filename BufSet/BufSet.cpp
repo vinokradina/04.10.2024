235bf7fb910d6c6febc064d12a9dad5f477be473
217010 <217010@KB37-302-C05>
2024-10-11 10:12:18 +0300

    Добавьте файлы проекта.

---
+++ BufSet/BufSet.cpp
@@ -0,0 +1,20 @@
+﻿#include <windows.h> 
+#include <iostream>
+int main()
+{
+	COORD coord; 
+	HANDLE hStdout; 
+	
+	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+	
+	std::cout << "Enter new screen buffer size." << std::endl; std::cout << "A number of columns: ";
+	std::cin >> coord.X;
+	std::cout << "A number of rows: ";
+	std::cin >> coord.Y;
+	
+	if (!SetConsoleScreenBufferSize(hStdout, coord))
+	{
+		std::cout << "Set console screen buffer size failed." << std::endl; return GetLastError();
+	}
+	return 0;
+}
