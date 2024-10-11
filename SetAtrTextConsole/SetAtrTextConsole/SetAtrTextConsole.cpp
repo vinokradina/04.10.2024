622a31db858a7925bfb6513b8b65377180497c78
vinokradina <106469305+vinokradina@users.noreply.github.com>
2024-10-11 12:15:38 +0300

    Add files via upload

---
+++ SetAtrTextConsole/SetAtrTextConsole/SetAtrTextConsole.cpp
@@ -0,0 +1,24 @@
+﻿#include <windows.h>
+#include <iostream> 
+using namespace std; 
+int main()
+{
+	char c;
+	HANDLE hStdout;
+	WORD wAttribute;
+	cout << "In order to set text attributes, input any char: ";
+	cin >> c;
+
+	hStdout = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	wAttribute = BACKGROUND_GREEN | BACKGROUND_INTENSITY |
+		FOREGROUND_RED | FOREGROUND_INTENSITY;
+
+	if (!SetConsoleTextAttribute(hStdout, wAttribute))
+	{
+		cout << "Set console text attribute failed." << endl;
+		return GetLastError();
+	}
+	cout << "The text attributes was changed." << endl;
+	return 0;
+}
